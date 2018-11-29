c70f3ad4f13c3fa21a8180ad66477c6fd8907a80
liyuan_zhu@pinssible.com <>
2018-11-29 19:42:51 +0800

    fix install

---
+++ build/rollup.config.js
@@ -0,0 +1,37 @@
+// rollup.config.js
+import vue from 'rollup-plugin-vue'
+import babel from 'rollup-plugin-babel'
+import postcss from 'rollup-plugin-postcss'
+import url from 'rollup-plugin-url'
+import uglify from 'rollup-plugin-uglify-es'
+import minimist from 'minimist'
+
+const argv = minimist(process.argv.slice(2))
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    name: 'VueVirtualTable',
+    exports: 'named'
+  },
+  plugins: [
+    vue({
+      css: true,
+      compileTemplate: true
+    }),
+    postcss({
+      plugins: []
+    }),
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**'
+    })
+  ]
+}
+
+// Only minify browser (iife) version
+if (argv.format === 'iife') {
+  config.plugins.push(uglify())
+}
+
+export default config
